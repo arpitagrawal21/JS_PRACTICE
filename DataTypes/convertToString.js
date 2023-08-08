926d54652243a7085fafd8c241e0bebd4e483a9d
Arpit Agrawal <105047748+arpitagrawal21@users.noreply.github.com>
2023-08-08 19:18:26 +0000

    convert data to string

---
+++ DataTypes/convertToString.js
@@ -0,0 +1,16 @@
+let text = 123;
+
+let str = String(text);
+console.log(str);
+
+text = true;
+str= String(text);
+console.log(str);
+
+text = null;
+str= String(text);
+console.log(str);
+
+text = undefined;
+str= String(text);
+console.log(str);
